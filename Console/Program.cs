1b250aaf02729f4265762e6acae2750d095d48af
josep <josep@VP-44602-S2>
2025-03-27 19:04:22 -0500

    Power

--- Console/Program.cs
+++ Console/Program.cs
@@ -86,6 +86,9 @@ public class Program
 	// Implement this method following a similar pattern as above
 	public static double Power(string x, string y)
 	{
-		return 0.0;
+		double baseNum = double.Parse(x);
+		double exponent = double.Parse(y);
+
+		return Math.Pow(baseNum, exponent);
 	}
 }
